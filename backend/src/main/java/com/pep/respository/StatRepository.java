8f50bcde5e9cc5ef3840a6f9285cd64f7158a3e5
Hemnath P <hemnathyogeshwaran@gmail.com>
2025-09-07 14:00:10 +0530

    StatRepository.java

---
+++ backend/src/main/java/com/pep/respository/StatRepository.java
@@ -0,0 +1,6 @@
+package com.pep.repository;
+
+import com.pep.model.Stat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StatRepository extends JpaRepository<Stat, Long> {}
